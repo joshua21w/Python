3af3515667c510acc95fdf0087530161da76c298
Joshua Ward <joshuaward@Joshuas-MacBook-Air.local>
2025-01-03 11:41:09 +0000

    ListComp script

--- Chap01/ListComp.py
+++ Chap01/ListComp.py
@@ -1,4 +1,6 @@
+#Create a large list
 large_list = [i for i in range(1_000_000)]
 
-listcomp = [x*x for x in large_list]
-print(listcomp)
+#Use list comp to multiply the number by itself
+list_comp = [x*x for x in large_list]
+print(list_comp)
